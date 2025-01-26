1c56c72613fc52ba08da3700c06446482bd0f251
teguh-budi2002 <budilaksono1102@gmail.com>
2025-01-26 20:22:31 +0700

    fix bugs

--- app/Http/Controllers/Dashboard/SolusiPenyakitController.php
+++ app/Http/Controllers/Dashboard/SolusiPenyakitController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Dashboard;
 
+use App\Http\Controllers\Controller;
 use App\Models\SolusiPenyakit;
 use Illuminate\Http\Request;
 
