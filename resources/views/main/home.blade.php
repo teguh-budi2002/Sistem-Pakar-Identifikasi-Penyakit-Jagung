4e02fd34fd4492159e94994cd23a8096d560ba96
teguh-budi2002 <budilaksono1102@gmail.com>
2025-01-29 19:31:10 +0700

    fix button can't touched

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -57,7 +57,7 @@
       </div>
       <div class="welcome_btn flex justify-center mt-5">
         @if (Auth::check())
-        <a href="{{ route('diagnose.index') }}" class="w-max p-2 px-4 flex items-center space-x-1 rounded-full bg-blue-700 hover:bg-blue-500 transition-colors duration-100 ease-in-out">
+        <a href="{{ route('diagnose.index') }}" class="w-max p-2 px-4 flex items-center space-x-1 rounded-full bg-blue-700 hover:bg-blue-500 transition-colors duration-100 ease-in-out z-50">
           <span class="text-white text-sm">Diagnosa Sekarang</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-white">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
