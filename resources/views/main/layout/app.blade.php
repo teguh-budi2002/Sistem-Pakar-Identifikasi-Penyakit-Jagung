9af686e0fc2b60c84844064b5bcc93a35b303c69
teguh-budi2002 <budilaksono1102@gmail.com>
2025-02-05 18:56:51 +0700

    adding favicon

--- resources/views/main/layout/app.blade.php
+++ resources/views/main/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>JagungQu | @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('images/logo/LOGO_Only.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
